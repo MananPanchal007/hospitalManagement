aaba5dfd30c6eb8a0c182b32b567f8c5a253edcd
MananPanchal007 <mananmp3232@gmail.com>
2025-12-24 11:11:20 -0500

    b

--- src/main/java/com/hospitalManagement/HospitalManagementApp.java
+++ src/main/java/com/hospitalManagement/HospitalManagementApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HospitalManagementProjectApplication {
+public class HospitalManagementApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HospitalManagementProjectApplication.class, args);
+		SpringApplication.run(HospitalManagementApp.class, args);
 	}
 
 }
